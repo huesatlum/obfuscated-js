4565f2fb7c3f1b783ef24af52f7a646629053d2e
huesatlum <huesatlum@gmail.com>
2015-07-01 12:31:33 -0500

    Create function-1.js

---
+++ function-1.js
@@ -0,0 +1,10 @@
+/* github.com/huesatlum | license: GPL | good luck! */
+f=function(i){p='g',T='.',R='harCode';G={g:function(d)
+{return d.replace(RegExp(T,p),y)},o:T};a=['orci',r=2];
+T=G[String[o='fromC'+R](7..toString(a[1]))];y=function
+(W,M){return W['con'+(typeof'con').replace(/...$/,'uc\
+tor')][o](W[(G+R)[Math[86E4.toString(31)](031)]+R+'At'
+]()-M)};a=a.concat((+T+(typeof+T).slice(-r--)).replace
+(/N/g,'\x70'));r=a.splice(r,r++,(!''+p)[--r]+G[p]('od\
+m'))[0];a.push(a.shift().split(/(..)/g)[G.g('rfxhvxk')
+]().join('\163'));l=a.indexOf(i);return~l&&a[++l%++r]}
